8cc5712bbb4fd265bbfc37da010d1395f38859ec
ecthelion5109 <xanderseling@gmail.com>
2026-05-19 14:19:29 -0300

    agregue sweet alerts y un swal al agregar un itemn

--- MyApd/src/components/OfertaCard/OfertaCard.jsx
+++ MyApd/src/components/OfertaCard/OfertaCard.jsx
@@ -1,14 +1,25 @@
 import Encabezado3 from "../Encabezado3"
 import styles from './OfertaCard.module.css'
+import Swal from 'sweetalert2'
+
 
 export default function OfertaCard({ offer }) {
+	function connectAgregarAPostulaciones() {
+		Swal.fire({
+			title: 'Agregada',
+			text: 'La oferta fue agregada a postulaciones pendientes',
+			icon: 'success',
+			confirmButtonText: 'Aceptar'
+		})
+	}
+
 	return (
 		<div className={styles.card}>
 			<Encabezado3>{offer.cargo}</Encabezado3>
 			<p>Distrito: {offer.descdistrito}</p>
 			<p>Turno: {offer.turno}</p>
 			<p>Modulos: {offer.hsmodulos}</p>
-			<button> Postularse</button>
+			<button onClick={connectAgregarAPostulaciones}> Agregar a postulaciones</button>
 		</div>
 	)
 }
