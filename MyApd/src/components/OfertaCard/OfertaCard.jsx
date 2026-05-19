315c626dabe44f1bb2d813f8f4abadd3ee7a4cd3
ecthelion5109 <xanderseling@gmail.com>
2026-05-19 14:34:08 -0300

    useState que hermoso lrpm.

--- MyApd/src/components/OfertaCard/OfertaCard.jsx
+++ MyApd/src/components/OfertaCard/OfertaCard.jsx
@@ -1,21 +1,33 @@
+import { useState } from 'react'
 import Encabezado3 from "../Encabezado3"
 import styles from './OfertaCard.module.css'
 import Swal from 'sweetalert2'
-
-
 export default function OfertaCard({ offer }) {
+	const [agregada, setAgregada] = useState(false)
 	function connectAgregarAPostulaciones() {
+		setAgregada(true)
 		Swal.fire({
 			title: 'Oferta agregada',
-			text: `${offer.cargo} en escuela ${offer.escuela} fue agregada a postulacionse pendientes`,
+			text: `${offer.cargo} en escuela ${offer.escuela} fue agregada a postulaciones pendientes`,
 			icon: 'success',
-			timer: 4500,
+			timer: 3500,
 			showConfirmButton: false,
 			toast: true,
 			position: 'top-end'
 		})
 	}
-
+	function connectEliminarPostulacion() {
+		setAgregada(false)
+		Swal.fire({
+			title: 'Oferta eliminada',
+			text: `${offer.cargo} fue eliminada de postulaciones pendientes`,
+			icon: 'info',
+			timer: 3500,
+			showConfirmButton: false,
+			toast: true,
+			position: 'top-end'
+		})
+	}
 	return (
 		<div className={styles.card}>
 			<Encabezado3>{offer.cargo}</Encabezado3>
@@ -23,7 +35,22 @@ export default function OfertaCard({ offer }) {
 			<p>Distrito: {offer.descdistrito}</p>
 			<p>Turno: {offer.turno}</p>
 			<p>Modulos: {offer.hsmodulos}</p>
-			<button onClick={connectAgregarAPostulaciones}> Agregar a postulaciones</button>
+			{
+				agregada ? (
+					<button
+						onClick={connectEliminarPostulacion}
+					>
+						Quitar de postulaciones
+					</button>
+				) : (
+					<button
+						onClick={connectAgregarAPostulaciones}
+					>
+						Agregar a postulaciones
+					</button>
+				)
+			}
+
 		</div>
 	)
 }
