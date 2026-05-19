583f9f30ecd53e9cb191bdaa582b1633b70bed3b
ecthelion5109 <xanderseling@gmail.com>
2026-05-18 22:03:25 -0300

    primer componente bra

---
+++ MyApd/src/components/Bienvenida.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Bienvenida() {
+
+	return <div>
+
+		<h1>¡Bienvenido al buscador de actos publicos!</h1>
+		<h2>Titulito h2</h2>
+		<p>Y un parrafito</p>
+	</div>
+
+
+}
+
+
+export default Bienvenida;
