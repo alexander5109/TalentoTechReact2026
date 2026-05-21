db7662f324115069363e7cc97979b89a2eddb3d8
ecthelion5109 <xanderseling@gmail.com>
2026-05-21 16:20:10 -0300

    setError, setCargando y algunas imagenes

--- MyApd/src/components/OfertaListContainer/OfertaListContainer.jsx
+++ MyApd/src/components/OfertaListContainer/OfertaListContainer.jsx
@@ -6,18 +6,26 @@ import { useEffect, useState } from 'react'
 
 export default function OfertaListContainer({ mensaje }) {
 
-	const [isLoading, setLoading] = useState(true)
+	const [errorExcept, setError] = useState(null)
+	const [isLoading, setCargando] = useState(true)
 	const [ofertas, setOfertas] = useState([])
 	useEffect(() => {
 
-		setLoading(true)
+		setError(null)
+		setCargando(true)
 		setTimeout(() => {
 			fetch('../../data/ofertas.json')
 				.then(response => response.json())
 				.then(data => {
 					setOfertas(data)
-					setLoading(false)
 				})
+				.catch(error => {
+					console.error("Ups. Hubo un error: ", error)
+					setError(error)
+				})
+				.finally(
+					setCargando(false)
+				)
 		}, 1000)
 
 	}, [])
@@ -26,7 +34,14 @@ export default function OfertaListContainer({ mensaje }) {
 		<div>
 			<Encabezado1>{mensaje}</Encabezado1>
 			{isLoading ? (
-				<p>Cargando ofertas...</p>
+				<div>
+					<h3>Cargando ofertas...</h3>
+				</div>
+			) : errorExcept ? (
+				<div>
+					<h3>Hubo un error. No se cargaran items</h3>
+					<p>El error es este: {errorExcept}</p>
+				</div>
 			) : (
 				<div className={styles.container}>
 					<OfertaList ofertas={ofertas} />
