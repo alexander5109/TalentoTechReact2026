6618a877449e8b1dfde05b1f8a62be621b8202ef
ecthelion5109 <xanderseling@gmail.com>
2026-05-21 15:41:41 -0300

    ok el json va a public resources y useEffect + useState para cargar el json async.

--- MyApd/src/components/OfertaListContainer/OfertaListContainer.jsx
+++ MyApd/src/components/OfertaListContainer/OfertaListContainer.jsx
@@ -1,9 +1,24 @@
 import OfertaList from '../OfertaList/OfertaList'
-import ofertas from "../../data/ofertas.json"
+// import ofertas from "../../data/ofertas.json"
 import Encabezado1 from '../Encabezado1'
 import styles from './OfertaListContainer.module.css'
+import { useEffect, useState } from 'react'
 
 export default function OfertaListContainer({ mensaje }) {
+
+	const [ofertas, setOfertas] = useState([])
+	useEffect(() => {
+
+		// setTimeout(() => {
+			fetch('../../data/ofertas.json')
+				.then(response => response.json())
+				.then(data => {
+					setOfertas(data)
+				})
+		// }, 1000)
+		
+	}, [])
+
 	return (
 		<div>
 			<Encabezado1>{mensaje}</Encabezado1>
