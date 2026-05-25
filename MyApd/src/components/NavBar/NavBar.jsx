d8e89ec97d97238401db7f2fd65faa4bb2e32c99
ecthelion5109 <xanderseling@gmail.com>
2026-05-25 02:45:41 -0300

    navbar hermosa. pseudo carrito widget chau.

--- MyApd/src/components/NavBar/NavBar.jsx
+++ MyApd/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,88 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+
 import styles from "./NavBar.module.css"
+
 import { usePendingPostulations } from "../../context/PendingPostulationsContext"
-import PendingPostulationsWidget from "./PendingPostulationsWidget/PendingPostulationsWidget"
 
 export default function NavBar() {
+
 	const { pendingPostulations } = usePendingPostulations()
+
 	return (
 		<nav className={styles.navbar}>
+
 			<div className={styles.logoSection}>
-				<Link to="/" className={styles.logo} > APD Finder </Link>
+				<NavLink
+					to="/"
+					className={styles.logo}
+				>
+					APD Finder
+				</NavLink>
 			</div>
+
 			<div className={styles.links}>
-				<Link to="/" className={styles.link}> Inicio </Link>
-				<Link to="/about" className={styles.link}> About us </Link>
-				<Link to="/contacto" className={styles.link}> Contacto </Link>
-				<Link to="/ofertas" className={styles.link}> Ofertas </Link>
-				{/* <Link to="/miPerfil" className={styles.link}> Mi Perfil </Link> */}
+
+				<NavLink
+					to="/"
+					className={({ isActive }) =>
+						isActive
+							? `${styles.link} ${styles.active}`
+							: styles.link
+					}
+				>
+					Inicio
+				</NavLink>
+
+				<NavLink
+					to="/about"
+					className={({ isActive }) =>
+						isActive
+							? `${styles.link} ${styles.active}`
+							: styles.link
+					}
+				>
+					About us
+				</NavLink>
+
+				<NavLink
+					to="/contacto"
+					className={({ isActive }) =>
+						isActive
+							? `${styles.link} ${styles.active}`
+							: styles.link
+					}
+				>
+					Contacto
+				</NavLink>
+
+				<NavLink
+					to="/ofertas"
+					className={({ isActive }) =>
+						isActive
+							? `${styles.link} ${styles.active}`
+							: styles.link
+					}
+				>
+					Ofertas
+				</NavLink>
+
 			</div>
-			<div className={styles.widget}>
-				<Link to="/pendingPostulations">
-					<PendingPostulationsWidget pendingCount={pendingPostulations.length} />
-				</Link>
+
+			<div className={styles.rightSection}>
+
+				<NavLink
+					to="/pendingPostulations"
+					className={({ isActive }) =>
+						isActive
+							? `${styles.pendingButton} ${styles.activePending}`
+							: styles.pendingButton
+					}
+				>
+					📝 Postulaciones ({pendingPostulations.length})
+				</NavLink>
+
 			</div>
+
 		</nav>
 	)
 }
--- MyApd/src/components/NavBar/PendingPostulationsWidget/PendingPostulationsWidget.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./PendingPostulationsWidget.module.css"
-
-export default function PendingPostulationsWidget({ pendingCount }) {
-	return (
-		<h3 className={styles.pendingWidget}>📝 Mis postulaciones({pendingCount})</h3>
-	)
-}
