5805776963631e0960d84d519e3ec40864456674
ecthelion5109 <xanderseling@gmail.com>
2026-05-25 01:12:58 -0300

    formulario hermoso

--- MyApd/src/components/Pages/ContactoPage/ContactoPage.jsx
+++ MyApd/src/components/Pages/ContactoPage/ContactoPage.jsx
@@ -1,9 +1,11 @@
-
-import PageSection from "../../common/PageSection/PageSection"
+import TextContainer from "../../common/TextContainer/TextContainer";
+import SectionTitleH3 from "../../common/SectionTitleH3/SectionTitleH3";
+import PrettyText from "../../common/PrettyText/PrettyText";
 
 import styles from "./ContactoPage.module.css"
 
 export default function ContactoPage() {
+
 	function manejarSubmit(e) {
 		e.preventDefault()
 		alert("Mensaje enviado")
@@ -11,32 +13,65 @@ export default function ContactoPage() {
 
 	return (
 		<>
-			<h1>Contacto</h1>
+			<TextContainer>
 
-			<PageSection>
+				<SectionTitleH3
+					upper="Contacto"
+					lower="Realiza tu consulta"
+				/>
+
+				<PrettyText>
+					Este proyecto se encuentra en desarrollo
+					constante y toda devolución resulta útil
+					para seguir mejorándolo. Si deseas realizar
+					una consulta, reportar un problema,
+					proponer una idea o simplemente compartir
+					tu experiencia utilizando APD Finder,
+					puedes hacerlo a través del siguiente
+					formulario.
+				</PrettyText>
+
+				<PrettyText>
+					El objetivo de esta herramienta es ayudar
+					a docentes a encontrar cargos de forma
+					más cómoda y eficiente, por lo que las
+					opiniones y sugerencias de la comunidad
+					resultan especialmente valiosas.
+				</PrettyText>
 
 				<form
 					className={styles.form}
 					onSubmit={manejarSubmit}
 				>
 
-					<label>Nombre</label>
-					<input
-						type="text"
-						placeholder="Tu nombre"
-					/>
+					<div className={styles.field}>
+						<label>Nombre</label>
 
-					<label>Email</label>
-					<input
-						type="email"
-						placeholder="tuemail@gmail.com"
-					/>
+						<input
+							type="text"
+							placeholder="Tu nombre"
+						/>
+					</div>
 
-					<label>Mensaje</label>
-					<textarea
-						rows="6"
-						placeholder="Escribe tu mensaje..."
-					/>
+
+					<div className={styles.field}>
+						<label>Email</label>
+
+						<input
+							type="email"
+							placeholder="tuemail@gmail.com"
+						/>
+					</div>
+
+
+					<div className={styles.field}>
+						<label>Mensaje</label>
+
+						<textarea
+							rows="6"
+							placeholder="Escribe tu mensaje..."
+						/>
+					</div>
 
 					<button type="submit">
 						Enviar mensaje
@@ -44,7 +79,7 @@ export default function ContactoPage() {
 
 				</form>
 
-			</PageSection>
+			</TextContainer>
 		</>
 	)
 }
